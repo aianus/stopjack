d89332a952ea736b1a89c12a5f5e365cf4940592
Alex Ianus <hire@alexianus.com>
2019-09-25 19:02:20 -0400

    Implement email forwarding of SMS messages

--- functions/forward_sms.js
+++ functions/forward_sms.js
@@ -1,10 +1,32 @@
-exports.handler = function(context, event, callback) {
+const sgMail = require('@sendgrid/mail');
+
+function forwardSMSviaSMSTwiml(context, event) {
   let twiml = new Twilio.twiml.MessagingResponse();
   twiml.message(`From: ${event.From}. Body: ${event.Body}`, {
     to: context.SMS_FORWARDING_NUMBER
   });
+  return twiml;
+}
 
-  // TODO: SendGrid email
+function forwardSMSviaEmail(context, event) {
+  sgMail.setApiKey(context.SENDGRID_API_KEY);
+  const msg = {
+    to: context.SMS_FORWARDING_TO_EMAIL,
+    from: context.SMS_FORWARDING_FROM_EMAIL,
+    subject: `New SMS message from: ${event.From}`,
+    text: event.Body
+  };
+  return sgMail.send(msg);
+}
 
-  callback(null, twiml);
+exports.handler = function(context, event, callback) {
+  let twiml = forwardSMSviaSMSTwiml(context, event);
+
+  forwardSMSviaEmail(context, event)
+    .then(function() {
+      callback(null, twiml);
+    })
+    .catch(function(err) {
+      callback(err);
+    })
 };
