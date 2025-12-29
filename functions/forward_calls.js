0fdb2b031043eaac311a99e8147d0381ae284ad0
Alex Ianus <hire@alexianus.com>
2025-12-29 16:15:06 +0800

    Allow specifying the caller id for forwarded calls

--- functions/forward_calls.js
+++ functions/forward_calls.js
@@ -1,5 +1,5 @@
-exports.handler = function(context, event, callback) {
-  let twiml = new Twilio.twiml.VoiceResponse()
-  twiml.dial(context.CALL_FORWARDING_NUMBER);
+exports.handler = function (context, event, callback) {
+  let twiml = new Twilio.twiml.VoiceResponse();
+  twiml.dial({ callerId: context.CALL_FORWARDING_CALLER_ID }, context.CALL_FORWARDING_NUMBER);
   callback(null, twiml);
 };
